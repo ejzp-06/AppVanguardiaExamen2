7f70c0a862cf3cef7c4103340094aa0dc3b187a1
ejzp-06 <ejzp06@gmail.com>
2020-12-16 23:57:52 -0600

    unit tests

---
+++ XUnitTest/Fake/FakePanelRepository.cs
@@ -0,0 +1,43 @@
+﻿using SolarEnergySystem.Core.Entities;
+using SolarEnergySystem.Core.Enums;
+using SolarEnergySystem.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace XUnitTest.Fake
+{
+    public class FakePanelRepository : IPanelRepository
+    {
+        private readonly IEnumerable<Panel> _panels;
+
+        public FakePanelRepository()
+        {
+            _panels = new List<Panel>()
+            {
+                new Panel
+                {
+                    Id = "ADQWE`",
+                    PanelType = PanelType.Regular
+                },
+
+                new Panel
+                {
+                    Id = "ADQWE1",
+                    PanelType = PanelType.Limited
+                },
+                new Panel
+                {
+                    Id = "ADQWE2",   
+                    PanelType = PanelType.Ultimate
+                },
+            };
+        }
+
+        public IEnumerable<Panel> GetPanelsByDescendingOrder()
+        {
+            return _panels.OrderByDescending(p => p.PanelType).ToList();
+        }
+    }
+}
---
+++ XUnitTest/PanelServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using SolarEnergySystem.Core.Enums;
+using SolarEnergySystem.Core.Interfaces;
+using SolarEnergySystem.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XUnitTest.Fake;
+
+namespace XUnitTest
+{
+    public class PanelServiceTest
+    {
+        private PanelService GetPanelService()
+        {
+            return new PanelService(new FakePanelRepository());
+        }
+
+        [Fact]
+        public void GetPanelsByDescendingOrder()
+        {
+            var service = GetPanelService();
+
+            var result = service.GetPanelsByDescedingOrder();
+
+            Assert.True(result.ResponseCode == ResponseCode.Success);
+            Assert.NotNull(result);
+        }
+    }
+}
