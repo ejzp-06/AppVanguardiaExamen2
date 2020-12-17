d2070551e36d9bfeffd5d95e81658f1b65172941
ejzp-06 <ejzp06@gmail.com>
2020-12-17 00:49:05 -0600

    ultima unit test - Post examen

---
+++ XUnitTest/Fake/FakeElectricityReadingRepository.cs
@@ -0,0 +1,51 @@
+﻿using SolarEnergySystem.Core.Entities;
+using SolarEnergySystem.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace XUnitTest.Fake
+{
+    public class FakeElectricityReadingRepository : IElectricityReadingRepository
+    {
+        private readonly IEnumerable<ElectricityReading> _electricityReadings;
+
+        public FakeElectricityReadingRepository()
+        {
+            _electricityReadings = new List<ElectricityReading>
+            {
+                new ElectricityReading
+                {
+                    Id = 1,
+                    ReadingDateTime = DateTime.UtcNow,
+                    Panel = new Panel
+                    {
+                        Id = "LOPPP"
+                    }
+                },
+
+                 new ElectricityReading
+                {
+                    Id = 1,
+                    ReadingDateTime = DateTime.UtcNow.AddDays(1),
+                    Panel = new Panel
+                    {
+                        Id = "LOPPP"
+                    }
+                },
+            };
+        }
+
+
+        public ElectricityReading GetRecentReadingByPanelId(string panelId)
+        {
+            return _electricityReadings.Where(r => r.Panel.Id == panelId).OrderByDescending(r => r.ReadingDateTime).FirstOrDefault();
+        }
+
+        public ElectricityReading RegisterReding(ElectricityReading electricityReading)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- XUnitTest/PanelServiceTest.cs
+++ XUnitTest/PanelServiceTest.cs
@@ -16,12 +16,29 @@ namespace XUnitTest
             return new PanelService(new FakePanelRepository());
         }
 
+        private ElectricityReadingService GetElectricityReadingService()
+        {
+            return new ElectricityReadingService(new FakeElectricityReadingRepository());
+        }
+
         [Fact]
         public void GetPanelsByDescendingOrder()
         {
             var service = GetPanelService();
 
             var result = service.GetPanelsByDescedingOrder();
+
+            Assert.True(result.ResponseCode == ResponseCode.Success);
+            Assert.NotNull(result);
+        }
+
+        [Theory]
+        [InlineData("LOPPP")]
+        public void GetRecentReadingByPanelId(string id)
+        {
+            var service = GetElectricityReadingService();
+
+            var result = service.GetRecentReadingByPanelId(id);
 
             Assert.True(result.ResponseCode == ResponseCode.Success);
             Assert.NotNull(result);
